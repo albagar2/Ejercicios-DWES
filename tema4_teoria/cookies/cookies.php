ee77b3b31776f619c9318308ec9281a3dcf8a9ee
albagar2 <baciapez@gmail.com>
2025-11-11 18:15:49 +0100

    Primer commit: subida inicial del proyecto PHP

---
+++ tema4_teoria/cookies/cookies.php
@@ -0,0 +1,11 @@
+<?php
+    //usamos el setcookie
+    setcookie("nombre","Pepe", time()+60);
+    echo 'hola';
+    
+    
+    ?>
+
+
+    <!--para usarla hay que hacer una peticion al servidor-->
+    <a href="datos.php">Ir a datos</a>
